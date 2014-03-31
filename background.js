c3f077e87148ee12c5c5a748ebdf813fcc98753c
tdsparrow <sqallowlee@gmail.com>
2014-03-31 23:30:56 +0800

    add content scripts and popup page

--- background.js
+++ background.js
@@ -1,4 +1,15 @@
-chrome.tabs.onSelectionChanged.addListener(function(tabId) {
-  lastTabId = tabId;
-  chrome.pageAction.show(lastTabId);
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+  lastTabId = activeInfo.tabId;
+  chrome.tabs.get(lastTabId, function(tab) {
+    if (tab.url.indexOf("http://list.lufax.com") == 0) {
+      chrome.pageAction.show(lastTabId);
+    }
+  });
 });
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (tab.url.indexOf("http://list.lufax.com") == 0) {
+    chrome.pageAction.show(lastTabId);
+  }
+});
+
